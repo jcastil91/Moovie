e900c2125bc52639207c23110618624015e04a23
Jose Luis Castillo <Jose@jose-luis-castillos-macbook-pro.local>
2013-02-21 06:10:00 -0500

    heroku

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
   attr_accessible :description, :image, :name, :average_rating
   has_many :reviews
+  
+  validates :name, :presence => true
+  validates :description, :presence => true
+  validates :image, :presence => true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,9 @@ class Review < ActiveRecord::Base
   belongs_to :movie
   belongs_to :user
   attr_accessible :content, :name, :rating, :user_id
+  
+  validates :user_id, :presence => true
+  validates :rating, :presence => true
+  validates :content, :presence => true
+  validates :name, :presence => true
 end
