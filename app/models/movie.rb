23505613dcdd5235d2c188618437e9265d120ab1
Jose Luis Castillo <Jose@jose-luis-castillos-macbook-pro.local>
2013-02-21 07:12:48 -0500

    i

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,6 @@ class Movie < ActiveRecord::Base
   attr_accessible :description, :image, :name, :average_rating
   has_many :reviews
   
-  validates :name, :presence => true
   validates :description, :presence => true
   validates :image, :presence => true
 end
